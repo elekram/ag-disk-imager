bc57ad78676a69a44892ce55e7bd3664bc2f93e3
Mark Lee <mlee.aus@gmail.com>
2020-05-17 12:36:50 +1000

    added code to find internal disk drive to make sure script does not wipe the booted usb drive

--- ag-disk-imager.ps1
+++ ag-disk-imager.ps1
@@ -20,4 +20,21 @@ function Test-DriversForMachineModelExist{
   Write-Host ""
 }
 
+function Get-InternalDiskNumber {
+  $internalDisks = Get-WmiObject win32_diskdrive | Where-Object {$_.interfacetype -ne "USB"}
+  $InternalDiskNumbers = @()
+
+  $internalDisks | ForEach-Object {
+    $InternalDiskNumbers += $_.DeviceID.Substring($_.DeviceID.Length -1)
+  }
+  if ($InternalDiskNumbers.Count -gt 1){
+    throw "ERROR: Found more than one internal disk. Script must exit."
+  }
+
+  if ($InternalDiskNumbers.Count -eq 0) {
+    throw "ERROR: No internal disk found for imaging. Script must exit."
+  }
+  return $InternalDiskNumbers[0]
+}
+
 main
