41887ff09da711e1e1cd1b30f46a40a574c55a08
Mark Lee <mlee.aus@gmail.com>
2020-05-17 12:34:26 +1000

    added main function to run sub functions and code to test whether driver for machine model exist

--- ag-disk-imager.ps1
+++ ag-disk-imager.ps1
@@ -1,6 +1,23 @@
+function main {
+  Get-MachineModel
+  Test-DriversForMachineModelExist
+}
+
 function Get-MachineModel{
   Write-Host "`n[ Retrieving computer model... ]" -ForegroundColor Cyan
   $script:machineModel = Get-ComputerInfo | Select-Object -ExpandProperty "csmodel*"
   $script:machineModel = "20G80001AU"
   Write-Host "[ >> Found $script:machineModel << ]" -ForegroundColor DarkYellow
 }
+function Test-DriversForMachineModelExist{
+  Get-Location | Write-Host
+  Write-Host "`n[ Checking for drivers... ]" -ForegroundColor Cyan
+  [bool] $isDriversForModel = Test-Path -Path "$script:driversPath\$script:machineModel"
+  if($isDriversForModel -ne 1){
+    throw "ERROR: No drivers for $script:machineModel found. Script must exit."
+  }
+  Write-Host "`n[ >> Found drivers for $script:machineModel << ]" -ForegroundColor DarkYellow
+  Write-Host ""
+}
+
+main
